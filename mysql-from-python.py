d1b445de2e07130fdb012be1f89fb734b621830b
ElisabethCheston <elisabeth.cheston@gmail.com>
2021-03-01 19:18:31 +0000

    Add and delete Bob

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -14,9 +14,7 @@ connection = pymysql.connect(host='localhost',
 
 try:
     with connection.cursor() as cursor:
-        cursor.execute("""CREATE TABLE IF NOT EXISTS
-                          Friends(name char(20), age int, DOB datetime);""")
-        # Note that the above will still display a warning (not error) if the
-        # table already exists
+        rows = cursor.execute("DELETE FROM Friends WHERE name = %s;", 'bob')
+        connection.commit()
 finally:
     connection.close()
